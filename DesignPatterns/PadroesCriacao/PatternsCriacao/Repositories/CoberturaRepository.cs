ae5e4361f9d5b699dc62587916f5952a4aff2b7c
Andre Granemann <softwaretech0210@gmail.com>
2023-12-18 00:24:57 -0300

    Subindo base servicos

---
+++ DesignPatterns/PadroesCriacao/PatternsCriacao/Repositories/CoberturaRepository.cs
@@ -0,0 +1,22 @@
+﻿using PadroesCriacao.PatternsCriacao.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadroesCriacao.PatternsCriacao.Repositories
+{
+    public class CoberturaRepository:IServico
+    {
+        public CoberturaRepository()
+        {
+            Console.WriteLine("Cobertura");
+        }
+
+        public void ExecutaCobranca()
+        {
+            
+        }
+    }
+}
